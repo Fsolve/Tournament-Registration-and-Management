9b5867e198f8a9fbdbbb6f5ff0ec4df18d39b3ea
Fsolve <ksalvina@yahoo.com>
2024-04-27 07:00:12 -0700

    delete comments

--- base/admin.py
+++ base/admin.py
@@ -6,14 +6,3 @@ admin.site.register(Game)
 admin.site.register(Participation)
 admin.site.register(Team)
 
-# luka
-# luka7532
-
-# salma
-# sohane7532
-
-# abdel
-# gamba7532
-
-# doja
-# catdoja7532
